456261eecd0ed8047449f996d8aba89678bd43e3
Pei Liu <peixiong.liu@gmail.com>
2014-06-27 19:45:32 -0400

    Add specs for games#index

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
 describe GamesController do
+  let(:user) { create :user }
+  let(:game) { create :game }
+  let(:completed_game) { create :completed_game }
+
+  describe "GET #index" do
+    it 'assigns all incomplete games to @games' do
+      get :index
+      expect(assigns(:games)).to eq [game]
+    end
+  end
 end
