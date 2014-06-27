af708adcf4e7e3979782cbc9a793f2e67a835e01
Stephen Roth & Pei Liu <pair+flamdoodle.girb0t@devbootcamp.com>
2014-06-27 18:24:13 -0400

    Create routes for games, users, rounds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 WhatsThis::Application.routes.draw do
 
+  root 'games#index' # Home page.  Has all in progress games.
+
+  post '/games' to: 'games#create' # Create a new game in database
+  get '/games/completed' to: 'games#completed' # View a list of all completed games
+
+  get '/users/games' to: 'users#games' # User can only see games they've been in, hence no id
+  post '/users' to: 'users#create' # Sign up.  Use ajax to post to controller.
+  post '/users/login' to: 'users#login' # User login.  Ajax post creates new session.
+  get '/users/logout' to: 'users#logout'
+
+  post '/rounds' to: 'rounds#create' # Creates a new round and adds it to a game
+
 end
