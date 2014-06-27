18981a71deb660dd009587b508fe2606d1295b01
Pei Liu <peixiong.liu@gmail.com>
2014-06-27 19:43:59 -0400

    Edit environment to silence rspec deprecation warnings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 WhatsThis::Application.initialize!
+
+RSpec.configure do |c|
+  c.expose_current_running_example_as :example
+end
