ceaa32d90e6cf2ed75fb1419b3b82250eb04b939
Pei Xiong Liu <peixiong.liu@gmail.com>
2014-06-27 00:37:58 -0400

    Add #is_complete? to Game model.

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,6 @@ class Game < ActiveRecord::Base
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
   has_many :rounds
 
-
   def is_complete?
     self.rounds.count >= 10 ? true : false
   end
