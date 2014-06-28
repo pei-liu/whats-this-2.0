69c847cb9cb6c7a95bc03d92bc9b61e0e7707c2c
Pei Liu <peixiong.liu@gmail.com>
2014-06-27 21:04:12 -0400

    Implement games#create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,6 +4,14 @@ class GamesController < ApplicationController
   end
 
   def create
+    if user_logged_in?
+      game = Game.create(user_id: current_user)
+      Round.create( game_id: game.id,
+                    user_id: current_user,
+                    content: params['round']['content'],
+                    content_type: params['round']['content_type'])
+    end
+    redirect_to root_path
   end
 
   def completed
