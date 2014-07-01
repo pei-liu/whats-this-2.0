323b05067986387aca87aff63d682a22e70a7e75
Pei Xiong Liu <peixiong.liu@gmail.com>
2014-07-01 19:52:25 -0400

    Add spec outline to users_controller_spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,41 @@
 require 'spec_helper'
 
 describe UsersController do
+
+  describe "POST create" do
+    context "with VALID attributes" do
+      it "creates a new user in the database"
+      it "saves the user id to session[:user_id]"
+    end
+
+    context "with INVALID attributes" do
+      it "does not create a new user in the database"
+    end
+  end
+
+  describe "POST login" do
+    context "with VALID credentials" do
+      it "saves the user id to sesison[:user_id]"
+    end
+
+    context "with INVALID credentials" do
+      it "does NOT save the user id to sesison[:user_id]"
+    end
+  end
+
+  describe "GET logout" do
+    it "clears the session"
+  end
+
+  describe "GET games" do
+    context "logged in" do
+      it "assigns to @created_games, games that the user started"
+      it "assigns to @played_games, games that the user participated in"
+    end
+
+    context "NOT logged in" do
+      it "redirects to homepage"
+    end
+  end
+
 end
