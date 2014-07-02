d8ef777baa0fc0976c0b4c6ee7e30a873b4f65c0
Pei Xiong Liu <peixiong.liu@gmail.com>
2014-07-01 21:20:45 -0400

    Implement UsersController#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   def create
+    # puts '*' * 100
+    new_user = User.new(user_params)
+    if new_user.save
+      session[:user_id] = new_user.id
+      render nothing: true
+    else
+      flash.now.alert = "Error"
+      render nothing: true
+    end
   end
 
   def login
@@ -10,4 +19,13 @@ class UsersController < ApplicationController
 
   def games
   end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
 end
