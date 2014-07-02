e353e0a3101a1b681bbc134ff1a3cff5127b4343
Pei Xiong Liu <peixiong.liu@gmail.com>
2014-07-01 21:18:29 -0400

    Factories now generate associated lists on create -Associated factories not working. Getting a 'username already exists error' when attempting to use :user_with_games_and_rounds factory. -Base factories are working

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -2,10 +2,21 @@ require 'faker'
 
 FactoryGirl.define do
   factory :game do
-    association :creator
+    creator
 
     factory :completed_game do
       is_complete true
+    end
+
+    factory :game_with_rounds do
+      ignore do
+        rounds_count 5
+      end
+
+      after(:create) do |game, evaluator|
+        # create_list(:round_drawing, evaluator.rounds_count, game: game)
+        create_list(:round_description, evaluator.rounds_count, game: game)
+      end
     end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,23 @@ FactoryGirl.define do
     username "Faker::Internet.username"
     password "yolo"
     password_confirmation "yolo"
+
+    factory :invalid_user do
+      username ""
+      password "yolo"
+      password_confirmation "yolo"
+    end
+
+    factory :user_with_games_and_rounds do
+      ignore do
+        rounds_count 5
+        games_count 5
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:game_with_rounds, evaluator.games_count, user: user)
+        create_list(:round_description, evaluator.rounds_count, user: user)
+      end
+    end
   end
 end
