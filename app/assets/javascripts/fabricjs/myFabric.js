f8fdb79a94dab412ae8abc087f20eeade0c3206a
Pei Xiong Liu <peixiong.liu@gmail.com>
2014-07-09 23:10:59 -0400

    Add color palette to canvas

--- app/assets/javascripts/fabricjs/myFabric.js
+++ app/assets/javascripts/fabricjs/myFabric.js
@@ -13,6 +13,13 @@ ready = function() {
     canvas.freeDrawingBrush.width = 11;
     canvas.freeDrawingBrush.color = "#fff";
 
+    $('.brush-color-controller .color-option').click(function(){
+        var color = $(this).css('background-color')
+        canvas.freeDrawingBrush.color = color
+        unglowAllBorders($('.brush-color-controller .color-option'))
+        glowBorder($(this))
+    })
+
     // $('#brush-width')[0].addEventListener('change', function(e) {
     //     canvas.freeDrawingBrush.width = 2 + parseInt($(this).val()) * 3;
     //     console.log("width: " + canvas.freeDrawingBrush.width)
@@ -30,3 +37,26 @@ ready = function() {
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
+
+
+var unglowAllBorders = function(selector) {
+    selector.removeAttr('outline');
+    selector.css('border-color', '#000')
+    selector.removeAttr('box-shadow')
+}
+
+var glowBorder = function(selector) {
+    selector.css('outline', 'none')
+    selector.css('border-color', '#9ecaed')
+    selector.css('box-shadow', '0 0 10px #9ecaed')
+}
+
+
+
+
+
+
+
+
+
+
