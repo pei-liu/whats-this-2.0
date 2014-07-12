fc4b304e863311eefd39adb602549967e70922fe
Pei Xiong Liu <peixiong.liu@gmail.com>
2014-07-11 23:24:52 -0400

    User can now contribute a drawing to an existing game -Write remaining tests for RoundsController#create_drawing

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -24,6 +24,13 @@ class RoundsController < ApplicationController
   def create_drawing
     svg = params["svg"]
     if session[:game_id] #This is an addition to an existing game
+      game = Game.find(session[:game_id])
+      Round.create( game_id: session[:game_id], 
+                    user_id: current_user,
+                    content: svg,
+                    content_type: "drawing")
+      game.check_finished
+      session.delete(:game_id)
     else #This is the first drawing of a new game
       game = Game.create(user_id: current_user)
       round = Round.create( game_id: game.id, 
